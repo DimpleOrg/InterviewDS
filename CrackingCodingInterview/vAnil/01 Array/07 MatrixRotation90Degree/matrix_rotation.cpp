98b3e48cde64ef2ad45ed0189d67bd8d807d72e7
anil.kumar4 <anil.kumar4@hp.com>
2018-09-09 21:57:09 +0530

    Adding Matrix rotation Version 2

--- CrackingCodingInterview/vAnil/01 Array/07 MatrixRotation90Degree/matrix_rotation.cpp
+++ CrackingCodingInterview/vAnil/01 Array/07 MatrixRotation90Degree/matrix_rotation.cpp
@@ -39,3 +39,105 @@ bool RotateMatrixby90Degree(int **matrix, int l)
 
 	return true;
 }
+
+
+int **matrix;
+int len;
+
+inline int* top(int layer, int index)
+{
+	if (index >= len || layer > len / 2)
+	{
+		throw;
+	}
+
+	int rIndex = 0;
+	int cIndex = 0;
+
+	rIndex = layer;
+	cIndex = layer + index;
+
+	return &matrix[rIndex][cIndex];
+}
+
+inline int* left(int layer, int index)
+{
+	if (index >= len || layer > len / 2)
+	{
+		throw;
+	}
+
+	int rIndex = 0;
+	int cIndex = 0;
+
+	rIndex = len-1-layer-index;
+	cIndex = layer;
+
+
+	return &matrix[rIndex][cIndex];
+}
+
+inline int* right(int layer, int index)
+{
+	if (index >= len || layer > len / 2)
+	{
+		throw;
+	}
+
+	int rIndex = 0;
+	int cIndex = 0;
+
+	rIndex = layer + index;
+	cIndex = len - 1 - layer;
+
+
+	return &matrix[rIndex][cIndex];
+}
+
+
+inline int* bottom(int layer, int index)
+{
+	if (index >= len || layer > len / 2)
+	{
+		throw;
+	}
+
+	int rIndex = 0;
+	int cIndex = 0;
+
+	rIndex = len - 1 - layer;
+	cIndex = len - 1 - layer - index;
+
+
+	return &matrix[rIndex][cIndex];
+}
+
+
+
+
+__declspec(dllexport) bool RotateMatrixBy90DegreeV2(int **matrix, int l)
+{
+	if (matrix == nullptr)
+		return false;
+
+	::matrix = matrix;
+	len = l;
+
+	for (int i = 0; i < l / 2; i++)
+	{
+		int layer = i;
+		int k = 0;
+		for (int j = i; j < l -1 - layer; j++)
+		{
+			int temp = *top(layer, k);
+			*top(layer, k) = *left(layer, k);
+			*left(layer, k) = *bottom(layer, k);
+			*bottom(layer, k) = *right(layer, k);
+			*right(layer, k) = temp;
+			k++;
+		}
+	}
+
+	return true;
+}
+
