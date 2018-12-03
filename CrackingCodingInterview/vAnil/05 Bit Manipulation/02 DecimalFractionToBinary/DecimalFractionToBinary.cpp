51a1a11c17925f9cf879ca84fad3963acc8c191b
anil.kumar4 <anil.kumar4@hp.com>
2018-12-03 12:17:49 +0530

    Merging resolutions for code conflicts

---
+++ CrackingCodingInterview/vAnil/05 Bit Manipulation/02 DecimalFractionToBinary/DecimalFractionToBinary.cpp
@@ -0,0 +1,38 @@
+#include "DecimalFractionToBinary.h"
+
+using namespace std;
+	
+MY02DECIMALFRACTIONTOBINARY_API std::string DecimalFractionToString(double decimalFraction)
+{
+	string result;
+	result.resize(32);
+
+	for (int i = 0; i < 32; i++)
+	{
+		if (decimalFraction == 0)
+		{
+			result[i] = '0';
+			break;
+		}
+		else
+		{
+			decimalFraction *= 2;
+
+			if (decimalFraction >= 1)
+			{
+				result[i] = '1';
+				decimalFraction -= 1;
+			}
+			else
+			{
+				result[i] = '0';
+			}
+		}
+	}
+
+	if (decimalFraction != 0)
+		throw string("Error");
+
+	return "0." + result;
+
+}
