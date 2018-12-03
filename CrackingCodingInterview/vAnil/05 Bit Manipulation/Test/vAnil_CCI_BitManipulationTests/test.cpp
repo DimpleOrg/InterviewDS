a9cc3b696e86b2e23f507d6a7a171a606d5156e0
anil.kumar4 <anil.kumar4@hp.com>
2018-12-03 12:13:30 +0530

    Adding code for DecimalFraction to binary

--- CrackingCodingInterview/vAnil/05 Bit Manipulation/Test/vAnil_CCI_BitManipulationTests/test.cpp
+++ CrackingCodingInterview/vAnil/05 Bit Manipulation/Test/vAnil_CCI_BitManipulationTests/test.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "../../01 Insertion_A/Insertion.h"
+#include "../../02 DecimalFractionToBinary/DecimalFractionToBinary.h"
 
 TEST(InsertionTest, Test1) {
 	std::string output;
@@ -12,4 +13,17 @@ TEST(InsertionTest, Test1) {
 
 
   EXPECT_EQ(result, 119);
+}
+
+TEST(DecimalFractionToBinaryString, Test1) {
+	std::string output;
+	testing::internal::CaptureStdout();
+
+
+	auto result = DecimalFractionToString(0.125);
+
+	output = testing::internal::GetCapturedStdout();
+
+
+	EXPECT_EQ(1, 1);
 }
